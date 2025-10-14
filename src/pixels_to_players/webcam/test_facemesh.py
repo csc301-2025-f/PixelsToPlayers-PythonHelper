479cb0729d105fc569ab0017bd6dd8d3c89f559d
doanmin6 <minh.doan@mail.utoronto.ca>
2025-10-14 11:55:53 -0400

    Update test_facemesh.py

--- src/pixels_to_players/webcam/test_facemesh.py
+++ src/pixels_to_players/webcam/test_facemesh.py
@@ -1,5 +1,6 @@
-from webcam.client import WebcamClient
-from webcam.processors import flip_horizontal, draw_facemesh
+from pixels_to_players.webcam.client import WebcamClient
+from pixels_to_players.webcam.processors import draw_facemesh, flip_horizontal
+
 
 with WebcamClient() as cam:
     cam.record(duration=10, processors=[flip_horizontal, draw_facemesh])
